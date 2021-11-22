06af263cc530d2901901eda7b084480d9e6af2c3
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-22 13:07:40 -0300

    Retirar redundancia.

--- model/Servicos.php
+++ model/Servicos.php
@@ -1,43 +1,33 @@
 <?php
 
     class Servico {
-        private $codServico;
-        private $codFuncionario;
-        private $codUsuario;
-        private $nomeServico;
-        private $descricaoServico;
-        private $valorServico;
-
-        public function setCodServico($cod) {
-            $this -> codServico = $cod;
+        public function buscarDados()
+        {
+            $res = array();
+            $cmd = $this->pdo->query("SELECT * FROM pessoa ORDER BY nome");
+            $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
+            return $res;
         }
-
-        public function getCodServico() {
-            return $this -> codServico;
-        }
-
-        public function setNomeServico($nome) {
-            $this -> nomeServico = $nome;
-        }
-
-        public function getNomeServico() {
-            return $this -> nomeServico;
-        }
-
-        public function setDescricaoServico($descricao) {
-            $this -> descricaoServico = $descricao;
-        }
-
-        public function getDescricaoServico() {
-            return $this -> descricaoServico;
-        }
-
-        public function setValorServico($valor) {
-            $this -> valorServico = $valor;
-        }
-
-        public function getValorServico() {
-            return $this -> valorServico;
+    
+        //FUNÇÃO PARA CADASTRAR PESSOAS NO BANCO DE DADOS
+        public function cadastrarServico($nome, $tempoEstimado, $valor)
+        {
+            //ANTES DE CADASTRAR VERIFICAR SE JA TEM O EMAIL CADASTRADO 
+            $cmd = $this->pdo->prepare("SELECT id FROM pessoa WHERE nome = :n");
+            $cmd->bindValue(":n", $nome);
+            $cmd->execute();
+            if ($cmd->rowCount() > 0) //email ja cadastrado no banco 
+            {
+                return false;
+            } else {
+                $cmd = $this->pdo->prepare("INSERT INTO pessoa (nome,tempoEstimado,valor)
+                VALUES(:n,:tmp,:vl)");
+                $cmd->bindValue(":n", $nome);
+                $cmd->bindValue(":tmp", $tempoEstimado);
+                $cmd->bindValue(":vl", $valor);
+                $cmd->execute();
+                return true;
+            }
         }
     }
 ?>
