84b7f97ecaea1946ab8d3cd64ada0a0cfc7a8c44
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-11-23 08:46:21 -0300

    Concluir o método cadastrarServico.

--- model/Servicos.php
+++ model/Servicos.php
@@ -1,33 +1,41 @@
 <?php
+require_once '../../conexao/conexao.php';
+?>
 
-    class Servico {
-        public function buscarDados()
+<?php
+class Servico
+{
+
+    public function buscarDados()
+    {
+        global $pdo;
+        $res = array();
+        $cmd = $pdo->query("SELECT * FROM servico ORDER BY nomeServico");
+        $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
+        return $res;
+    }
+
+    //FUNÇÃO PARA CADASTRAR PESSOAS NO BANCO DE DADOS
+    public function cadastrarServico($nome, $tempoEstimado, $valor)
+    {
+        global $pdo;
+
+        //ANTES DE CADASTRAR VERIFICAR SE JA TEM O EMAIL CADASTRADO 
+        $cmd = $pdo->prepare("SELECT codServico FROM servico WHERE nomeServico = :n");
+        $cmd->bindValue(":n", $nome);
+        $cmd->execute();
+        if ($cmd->rowCount() > 0) //email ja cadastrado no banco 
         {
-            $res = array();
-            $cmd = $this->pdo->query("SELECT * FROM servico ORDER BY nomeServico");
-            $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
-            return $res;
-        }
-    
-        //FUNÇÃO PARA CADASTRAR PESSOAS NO BANCO DE DADOS
-        public function cadastrarServico($nome, $tempoEstimado, $valor)
-        {
-            //ANTES DE CADASTRAR VERIFICAR SE JA TEM O EMAIL CADASTRADO 
-            $cmd = $this->pdo->prepare("SELECT codServico FROM servico WHERE nomeServico = :n");
-            $cmd->bindValue(":n", $nome);
-            $cmd->execute();
-            if ($cmd->rowCount() > 0) //email ja cadastrado no banco 
-            {
-                return false;
-            } else {
-                $cmd = $this->pdo->prepare("INSERT INTO servico (nomeServico,tempoEstimado,valor)
+            echo "<script>alert('Serviço já está cadastrado!');</script>";
+            return false;
+        } else {
+            $cmd = $pdo->prepare("INSERT INTO servico (nomeServico,tempoEstimado,valor)
                 VALUES(:n,:tmp,:vl)");
-                $cmd->bindValue(":n", $nome);
-                $cmd->bindValue(":tmp", $tempoEstimado);
-                $cmd->bindValue(":vl", $valor);
-                $cmd->execute();
-                return true;
-            }
+            $cmd->bindValue(":n", $nome);
+            $cmd->bindValue(":tmp", $tempoEstimado);
+            $cmd->bindValue(":vl", $valor);
+            $cmd->execute();
+            return true;
         }
     }
-?>
+}
